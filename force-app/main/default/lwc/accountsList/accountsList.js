dbea5b2334a7ce0462869e566cdad73b0d2aa71b
Eduardo Offermann Palma <eduardo.offermann@gocode.cl>
2022-04-08 21:39:21 -0400

    Complete development of accountList and contactList LWC components

--- force-app/main/default/lwc/accountsList/accountsList.js
+++ force-app/main/default/lwc/accountsList/accountsList.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getAccounts from '@salesforce/apex/AccountController.getAccounts';
 
-export default class AccountsList extends LightningElement {}
+import { publish, MessageContext } from 'lightning/messageService';
+import ACCOUNT_SELECTED_CHANNEL from '@salesforce/messageChannel/Account_Selected__c';
+export default class AccountsList extends LightningElement {
+
+	@wire(getAccounts)
+	accounts;
+	@wire(MessageContext)
+	messageContext;
+
+	fetchContacts(event){
+		event.preventDefault();
+		const index = parseInt(event.target.dataset.index),
+					payload = { account: this.accounts.data[index] };
+		publish(this.messageContext, ACCOUNT_SELECTED_CHANNEL, payload);
+	}
+
+}
--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,3 +1,37 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
 
-export default class ContactList extends LightningElement {}
+import {
+    subscribe,
+    unsubscribe,
+    APPLICATION_SCOPE,
+    MessageContext
+} from 'lightning/messageService';
+//import accountsMessageChannel from '@salesforce/messageChannel/accountsMessageChannel';
+import ACCOUNT_SELECTED_CHANNEL from '@salesforce/messageChannel/Account_Selected__c';
+
+export default class ContactList extends LightningElement {
+
+	@api selectedAccount = {Id: '', Name: ''};
+
+	@wire(MessageContext)
+	messageContext;
+	@wire(getContacts, { accountId: '$selectedAccount.Id' })
+	contacts
+
+	connectedCallback() {
+		if (!this.subscription) {
+			this.subscription = subscribe(
+				this.messageContext,
+				ACCOUNT_SELECTED_CHANNEL,
+				(message) => this.setSelectedAccount(message),
+				{ scope: APPLICATION_SCOPE }
+			);
+		}
+  }
+
+	setSelectedAccount(message){
+		this.selectedAccount = message.account;
+	}
+
+}
